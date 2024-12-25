d23fa9abb60d0f6816e01df4270aabbd5d1b13d1
giorgizena123 <zenaishvili621@gmail.com>
2024-12-25 15:59:46 +0400

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+class Rectangle extends Shape {
 
-public class Rectangle {
+    public Rectangle(double x, double y, String name) {
+        super(x, y, name);
+    }
+
+    @Override
+    public void printDetails() {
+        super.printDetails();
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (x + y);
+    }
 }
