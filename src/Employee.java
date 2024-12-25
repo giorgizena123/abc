85fdca265c105014f45fb0046a03861344ba34c2
giorgizena123 <zenaishvili621@gmail.com>
2024-12-25 15:58:13 +0400

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+abstract class Employee {
+    protected String firstName;
+    protected String lastName;
+    protected String phoneNumber;
 
-public class Employee {
+
+    public Employee(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    public abstract double calculateSalary();
+
+
+    public void printDetails() {
+        System.out.println("სახელი: " + firstName);
+        System.out.println("გვარი: " + lastName);
+        System.out.println("ტელეფონის ნომერი: " + phoneNumber);
+    }
 }
