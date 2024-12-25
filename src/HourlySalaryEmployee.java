f308a67114cc9497f59c2e9e78ddd9c0fa001010
giorgizena123 <zenaishvili621@gmail.com>
2024-12-25 15:58:47 +0400

    Update HourlySalaryEmployee.java

--- src/HourlySalaryEmployee.java
+++ src/HourlySalaryEmployee.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+class HourlySalaryEmployee extends Employee {
+    private int workedDays;
+    private double dailyRate;
 
-public class HourlySalaryEmployee {
+
+    public HourlySalaryEmployee(String firstName, String lastName, String phoneNumber, int workedDays, double dailyRate) {
+        super(firstName, lastName, phoneNumber);
+        this.workedDays = workedDays;
+        this.dailyRate = dailyRate;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return workedDays * dailyRate;
+    }
 }
