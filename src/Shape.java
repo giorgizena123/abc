e6c16b2a1e69348c0faf5a351aeed626f6246946
giorgizena123 <zenaishvili621@gmail.com>
2024-12-25 16:00:27 +0400

    Update Shape.java

--- src/Shape.java
+++ src/Shape.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+class Shape {
+    protected double x;
+    protected double y;
+    protected String name;
 
-public class Shape {
+    public Shape(double x, double y, String name) {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+
+    public void printDetails() {
+        System.out.println("ფიგურის სახელი: " + name);
+        System.out.println("x: " + x + ", y: " + y);
+    }
+
+    public double perimeter() {
+        return 0;
+    }
 }
