81662fdd4a08e7d93a0a21a2a4a79e8885b7798c
giorgizena123 <zenaishvili621@gmail.com>
2024-12-25 16:00:55 +0400

    Update Square.java

--- src/Square.java
+++ src/Square.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+class Square extends RegularPolygon {
 
-public class Square {
+
+    public Square(double length) {
+        super(4, length);
+    }
+
+    @Override
+    public double perimeter() {
+        return super.perimeter();
+    }
+}
+
+
+class EquilateralTriangle extends RegularPolygon {
+
+
+    public EquilateralTriangle(double length) {
+        super(3, length);
+    }
+
+    @Override
+    public double perimeter() {
+        return super.perimeter();
+    }
 }
