1b526f6c6ba9936255c3aa3015a34f0bda3d74a2
giorgizena123 <zenaishvili621@gmail.com>
2024-12-25 15:57:34 +0400

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+class Car extends Toy {
+    private double cabinPrice;
+    private double wheelPrice;
+    private int wheelCount;
 
-public class Car {
+
+    public Car(String name, double cabinPrice, double wheelPrice, int wheelCount, double additionalCost) {
+        super(name, additionalCost);
+        this.cabinPrice = cabinPrice;
+        this.wheelPrice = wheelPrice;
+        this.wheelCount = wheelCount;
+    }
+
+
+    @Override
+    public double calculatePrice() {
+        return cabinPrice + (wheelPrice * wheelCount) + additionalCost;
+    }
 }
