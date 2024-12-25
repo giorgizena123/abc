1f7c266c8ceb95cba5c8b019591d97a4a63bcbb8
giorgizena123 <zenaishvili621@gmail.com>
2024-12-25 16:01:51 +0400

    Update Vehicle.java

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+class Vehicle implements Drivable, Steerable {
 
-public class Vehicle {
+    @Override
+    public void startEngine() {
+        System.out.println("ძრავი დაიწყო.");
+    }
+
+    @Override
+    public void stopEngine() {
+        System.out.println("ძრავი გამორთულია.");
+    }
+
+    @Override
+    public void turnLeft() {
+        System.out.println("მანქანა მარცხნივ უბრუნდება.");
+    }
+
+    @Override
+    public void turnRight() {
+        System.out.println("მანქანა მარჯვნივ უბრუნდება.");
+    }
 }
